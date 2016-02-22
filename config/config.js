caf739342c958c80b0612d9f7eac33941d1c4005
Simon Oulevay <simon.oulevay@gmail.com>
2016-02-22 19:34:21 +0100

    Initial commit.

---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'webs-2016-example'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/webs-2016-example-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'webs-2016-example'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/webs-2016-example-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'webs-2016-example'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/webs-2016-example-production'
+  }
+};
+
+module.exports = config[env];
