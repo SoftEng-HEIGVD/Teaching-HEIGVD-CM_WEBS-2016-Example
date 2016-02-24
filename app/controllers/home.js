ab07b13279081cf83b84695580b095cdef222ddc
Simon Oulevay <simon.oulevay@gmail.com>
2016-02-24 21:18:57 +0100

    Test method.

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,6 @@
 var express = require('express'),
   router = express.Router(),
-  mongoose = require('mongoose'),
-  Article = mongoose.model('Article');
+  mongoose = require('mongoose');
 
 module.exports = function (app) {
   app.use('/', router);
@@ -16,4 +15,15 @@ router.get('/', function (req, res, next) {
 
   // To redirect to the APIDOC documentation instead, comment the 3 lines above and uncomment this one.
   //res.redirect('/apidoc');
+});
+
+router.all('/test/:param1/:param2', function(req, res, next) {
+  res.send({
+    method: req.method,
+    path: req.path,
+    params: req.params,
+    query: req.query,
+    headers: req.headers,
+    body: req.body
+  });
 });
